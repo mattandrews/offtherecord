7e762d546f4dfd238c1993aeafd8b9ba7ee5ed9c
Matt Andrews <matt@mattandrews.info>
2015-02-26 21:32:16 +0000

    new stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,3 +55,22 @@ var buttons = document.querySelectorAll('.js-vote');
 [].slice.call(buttons).forEach(function(b){
     b.addEventListener('click', vote);
 });
+
+var toggle = function (event) {
+    var elm = event.target;
+    var togglePane = elm.getAttribute('data-toggle');
+    var togglePaneElm = document.getElementById(togglePane);
+    if (togglePaneElm) {
+        togglePaneElm.classList.toggle('hidden');
+        elm.classList.add('hidden');
+    }
+};
+
+var togglers = document.querySelectorAll('.js-toggle');
+var togglePanes = document.querySelectorAll('.js-toggle-pane');
+[].slice.call(togglers).forEach(function(t){
+    t.addEventListener('click', toggle);
+});
+[].slice.call(togglePanes).forEach(function(t){
+    t.classList.add('hidden');
+});
